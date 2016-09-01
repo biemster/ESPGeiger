4ef9b665eca823da08c454c8040261d14939e4e1
Lars Beemster <l.j.beemster@gmail.com>
2016-09-01 11:58:32 +0200

    updated click() in main to click the buzzer

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from machine import Pin
 import ESPGeiger as geiger
 
 geiger.init()
 
+# buzzer is between pin14=D5 and pin12=D6
+D5 = Pin(14, Pin.OUT)
+D6 = Pin(12, Pin.OUT)
+
 def click():
-    # click the buzzer
+    # click the buzzer connected to pins pin14=D5 and pin12=D6
     print('.', end='')
+    D5.high()
+    D6.low()
+    time.sleep_ms(2) # use 4 or 5 to get a lower tone
+    D5.low()
+    D6.high()
+
+    # set pins low after click
+    time.sleep_ms(5) # this goes into dead time of the counter
+    D6.low()
 
 count = 0
 while True:
